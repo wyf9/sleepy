4ef8314cd3a77fee90beaf175da25158b0d09801
wyf9 <wyf9@wyf9.top>
2024-07-30 18:56:39 +0800

    edit start.py: add base path check

--- start.py
+++ start.py
@@ -3,12 +3,14 @@
 import os
 import time
 c = 0
+server = os.path.join(os.path.dirname(sys.argv[0]), 'server.py')
+print(f'Server path: {server}')
 while True:
     c += 1
     print(f'Starting server #{c}')
     if os.name == 'nt':
-        r = os.system('python server.py')
+        r = os.system(f'python {server}')
     else:
-        r = os.system('python3 server.py')
+        r = os.system(f'python3 {server}')
     print(f'#{c} exited with code {r}\nwaiting 5s')
     time.sleep(5)
