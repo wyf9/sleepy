cc2d9b15d62dae58890d92101170011539feedd8
wyf9 <wyf9@wyf9.top>
2024-07-30 17:39:26 +0800

    start.py 增加操作系统检测

--- start.py
+++ start.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-#coding: utf-8
+# coding: utf-8
 import os
 import time
 c = 0
 while True:
     c += 1
     print(f'Starting server #{c}')
-    r = os.system('python3 server.py')
+    if os.name == 'nt':
+        r = os.system('python server.py')
+    else:
+        r = os.system('python3 server.py')
     print(f'#{c} exited with code {r}\nwaiting 5s')
     time.sleep(5)
