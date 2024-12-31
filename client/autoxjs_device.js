494ba6eae52261f9bc7c073e4a02a316f4dec815
wyf9 <wyf9@wyf9.top>
2025-01-01 03:42:17 +0800

    client: `autoxjs_device.js` √

--- client/autoxjs_device.js
+++ client/autoxjs_device.js
@@ -14,7 +14,83 @@ const CHECK_INTERVAL = '3000'; // 检查间隔 (毫秒)
 
 auto.waitFor(); // 等待无障碍
 
-console.log(device.getBattery());
-console.log(device.isScreenOn());
-console.log(currentPackage());
-console.log(getAppName(currentPackage()));
+// 替换了 secret 的日志
+function log(msg) {
+    try {
+        console.log(msg.replace(SECRET, '[REPLACED]'));
+    } catch (e) {
+        console.log(msg);
+    }
+}
+function error(msg) {
+    try {
+        console.error(msg.replace(SECRET, '[REPLACED]'));
+    } catch (e) {
+        console.error(msg);
+    }
+}
+
+var last_status = '';
+
+function check_status() {
+    /*
+    检查状态并返回 app_name (如未在使用则返回空)
+    */
+    log(`[check] screen status: ${device.isScreenOn()}`);
+    if (!device.isScreenOn()) {
+        return ('');
+    }
+    var app_package = currentPackage(); // 应用包名
+    log(`[check] app_package: '${app_package}'`);
+    var app_name = app.getAppName(app_package); // 应用名称
+    log(`[check] app_name: '${app_name}'`);
+    var battery = device.getBattery(); // 电池百分比
+    log(`[check] battery: ${battery}%`);
+    var retname = `[${battery}%] ${app_name}`; // 返回
+    if (!app_name) {
+        retname = '';
+    }
+    return (retname);
+}
+function send_status() {
+    /*
+    发送 check_status() 的返回
+    */
+    var app_name = check_status();
+    log(`ret app_name: '${app_name}'`);
+
+    // 判断是否与上次相同
+    if (app_name == last_status) {
+        log('same as last status, bypass request');
+        return;
+    }
+    last_status = app_name;
+    // 判断 using
+    if (app_name == '') {
+        log('using: false');
+        var using = false;
+    } else {
+        log('using: true');
+        var using = true;
+    }
+
+    // POST to api
+    log(`POST ${API_URL}`);
+    r = http.postJson(API_URL, {
+        'secret': SECRET,
+        'id': ID,
+        'show_name': SHOW_NAME,
+        'using': using,
+        'app_name': app_name
+    });
+    log(`response: ${r.body.string()}`);
+}
+while (true) {
+    log('---------- Run\n');
+    try {
+        send_status();
+    } catch (e) {
+        error(`ERROR sending status: ${e}`);
+    }
+    sleep(CHECK_INTERVAL);
+}
