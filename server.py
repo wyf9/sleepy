adc85dee531a2a0ca8274294e9ab6a8d18f106db
wyf9 <wyf9@wyf9.top>
2025-07-07 20:46:25 +0800

    moonlight 支持获取系统主题设置 - 修复启动日志中 `Starting HTTP Server` IPv6 地址显示错误

--- server.py
+++ server.py
@@ -56,7 +56,6 @@ except:
 def showip():
     '''
     在日志中显示 ip, 并记录 metrics 信息
-    ~~如 Header 中 User-Agent 为 SleepyPlugin/(每次启动使用随机 uuid) 则不进行任何记录~~
 
     :param req: `flask.request` 对象, 用于取 ip
     :param msg: 信息 (一般是路径, 同时作为 metrics 的项名)
@@ -478,14 +477,15 @@ if __name__ == '__main__':
     #     pass
     # --- launch
     # 检查是否启用 HTTPS
+    listening = f'{f"[{env.main.host}]" if ":" in env.main.host else env.main.host}:{env.main.port}'
     if env.main.https_enabled:
         ssl_context = (env.main.ssl_cert, env.main.ssl_key)
-        u.info(f'Starting HTTPS server: {env.main.host}:{env.main.port}{" (debug enabled)" if env.main.debug else ""}')
+        u.info(f'Starting HTTPS server: https://{listening}{" (debug enabled)" if env.main.debug else ""}')
         u.info(f'Using SSL certificate: {env.main.ssl_cert}')
         u.info(f'Using SSL key: {env.main.ssl_key}')
     else:
         ssl_context = None
-        u.info(f'Starting HTTP server: {env.main.host}:{env.main.port}{" (debug enabled)" if env.main.debug else ""}')
+        u.info(f'Starting HTTP server: http://{listening}{" (debug enabled)" if env.main.debug else ""}')
 
     try:
         app.run(  # 启↗动↘
